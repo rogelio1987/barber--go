91993d5d3c909325e3763d918ec2aaf6215b8f30
Rogelio Queada <rogelio.quesada1987@gmail.com>
2023-06-30 15:25:05 -0400

    first

--- app.config.js
+++ app.config.js
@@ -1,7 +1,18 @@
-import 'dotenv/config'
+import 'dotenv/config';
+
+// export default {
+// 	extra: {
+// 		BASEURL: process.env.EXPO_BASEURL,
+// 	},
+// }
 
 export default {
-	extra: {
-		BASEURL: process.env.EXPO_BASEURL,
-	},
+  "expo": {
+    "extra": {
+      BASEURL: process.env.EXPO_BASEURL,
+      "eas": {
+        "projectId": "05d623a8-ff32-4a9a-979a-4f7db62ff7e9"
+      }
+    }
+  }
 }
